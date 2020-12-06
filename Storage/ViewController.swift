41b501caef7dd8225d94e1542e910824843b6c70
Tal <talspektor.gmail.com>
2020-12-06 13:29:50 +0200

    Initial Commit

---
+++ Storage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storage
+//
+//  Created by Tal Spektor on 06/12/2020.
+//  Copyright © 2020 Tal Spektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
